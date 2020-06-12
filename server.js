a58e6a1fd0c4bcf1aa9bb92acdf49be9247ebca0
wolfchen7663 <wolfchen7663@163.com>
2020-06-12 22:43:48 +0800

    update the server.js

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ mongoose.connect(process.env.DATABASE_URL, {
 });
 const db = mongoose.connection;
 db.on("error", error=>console.error(error));
-db.once("open", ()=>console.log("Connect to mongoose"));
+db.once("open", ()=>console.log("Connected to mongoose"));
 app.use("/", indexRouter);
 app.listen(process.env.PORT || 3000);
